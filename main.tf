ed7aed967dc9260cd6bdbd0da7ad023dc7150002
dhts1990 <dhts1990@gmail.com>
2025-04-05 13:14:10 +0800

    changed

--- main.tf
+++ main.tf
@@ -1,22 +1,55 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 terraform {
+  required_version = ">= 1.7.5"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
   backend "s3" {
     bucket = "sctp-ce9-tfstate"
-    key    = "huang-s3-tf-ci.tfstate" #Change this
+    key    = "huang-s3-tf-ci.tfstate"
     region = "us-east-1"
   }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
 
 data "aws_caller_identity" "current" {}
 
 locals {
-  name_prefix = split("/", "${data.aws_caller_identity.current.arn}")[1]
+  name_prefix = split("/", data.aws_caller_identity.current.arn)[1]
   account_id  = data.aws_caller_identity.current.account_id
 }
 
 resource "aws_s3_bucket" "s3_tf" {
   bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"
+
+  # Recommended security settings
+  force_destroy = false
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "s3_tf" {
+  bucket = aws_s3_bucket.s3_tf.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3_tf" {
+  bucket = aws_s3_bucket.s3_tf.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
 }
